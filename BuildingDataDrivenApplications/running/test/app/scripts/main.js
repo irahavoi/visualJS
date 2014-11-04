9a6aa0f855eb2dcc41f836f6d8354920e1596ac2
unknown <irahavoi@wpg02333.obsglobal.com>
2014-11-03 21:03:45 -0600

    Backbone app generated with yo

---
+++ BuildingDataDrivenApplications/running/test/app/scripts/main.js
@@ -0,0 +1,18 @@
+/*global Running, $*/
+
+
+window.Running = {
+    Models: {},
+    Collections: {},
+    Views: {},
+    Routers: {},
+    init: function () {
+        'use strict';
+        console.log('Hello from Backbone!');
+    }
+};
+
+$(document).ready(function () {
+    'use strict';
+    Running.init();
+});
